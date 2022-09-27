c7980e66b7ab2f137f456472b1a2589d225a5d4d
Smrinov <wmforum@bk.ru>
2022-09-27 23:13:42 +0300

    Update lesson 701983

--- OOP/lesson_701983.py
+++ OOP/lesson_701983.py
@@ -42,8 +42,8 @@ print(pt._Point__x)
 """
 
 """
-step 4
-"""
+# step 4
+
 
 
 class Clock:
@@ -66,3 +66,32 @@ class Clock:
 
 clock = Clock(4530)
 print()
+"""
+# step 5
+class Money:
+    def __init__(self, money):
+        self.__money = 0
+        if self.__check_money(money):
+            self.__money = money
+
+    def set_money(self, money):
+        if self.__check_money(money):
+            self.__money = money
+
+    def get_money(self):
+        return self.__money
+
+    def add_money(self, mn):
+        self.__money += mn.get_money()
+
+    @classmethod
+    def __check_money(cls, money):
+        return isinstance(money, int) and money >= 0
+
+mn_1 = Money(10)
+mn_2 = Money(20)
+mn_1.set_money(100)
+mn_2.add_money(mn_1)
+m1 = mn_1.get_money()    # 100
+m2 = mn_2.get_money()    # 120
+pass
