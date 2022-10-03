71c03a77a7d250ee863fa2ebb8c65818cbce1301
KirillNN <wmforum@bk.ru>
2022-10-03 18:47:35 +0300

    Update lesson 265077

--- lesson/lesson_265077.py
+++ lesson/lesson_265077.py
@@ -8,6 +8,42 @@ print('Привет,', input())
 print(input(), '- чемпион!')
 
 print(input(), input(), input(), sep='\n')
+
+x, y, z = input(), input(), input()
+print(z, y, x, sep='\n')
+
+print('Mercury', 'Venus', sep='*', end='!')
+print('Mars', 'Jupiter', sep='**', end='?')
+
+print('a', 'b', 'c', sep='*')
+print('d', 'e', 'f', sep='**', end='')
+print('g', 'h', 'i', sep='+', end='%')
+print('j', 'k', 'l', sep='-', end='\n')
+print('m', 'n', 'o', sep='/', end='!')
+print('p', 'q', 'r', sep='1', end='%')
+print('s', 't', 'u', sep='&', end='\n')
+print('v', 'w', 'x', sep='%')
+print('y', 'z', sep='/', end='!')
+
+print('I', 'like', 'Python', sep='***')
+
+s, x, y, z = input(), input(), input(), input()
+print(x, y, z, sep=s)
+
+print('Привет,', input(), end='!')
+
+s1 = 'C++'
+s2 = 'Python'
+s3 = 'Java'
+s3 = s2
+s1 = s3
+print(s1)
 """
-
-
+# print('Java')
+# print('Ruby')
+# print('Scala')
+print('Python', end='+')  # print('C++')
+# print('GO')
+print('C#', end='=')  # print('C')
+print('awesome')
+# finish
