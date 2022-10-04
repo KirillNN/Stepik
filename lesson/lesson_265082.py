8f07edcb87fa122c0d25d676698119a273d8b881
KirillNN <wmforum@bk.ru>
2022-10-04 17:27:48 +0300

    Update lesson 265082

--- lesson/lesson_265082.py
+++ lesson/lesson_265082.py
@@ -6,7 +6,7 @@ print(['Равносторонний', 'Равнобедренный', 'Разн
 
 x = map(int, (input() for _ in range(3)))
 print(sorted(x)[1])
-"""
+
 x = int(input())
 if x in (1, 3, 5, 7, 8, 10, 12):
     print(31)
@@ -14,3 +14,12 @@ elif x == 2:
     print(28)
 else:
     print(30)
+
+weight = int(input())
+print(["Легкий вес", "Первый полусредний вес", "Полусредний вес"][(weight >= 60) + (weight >= 64) + (weight >= 69)])
+"""
+a, b, zn = int(input()), int(input()), input()
+zn_good = '+-*/'
+if zn not in zn_good:
+    print('Неверная операция')
+else:
