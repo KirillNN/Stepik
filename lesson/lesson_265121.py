aac21adf32c246e8dffedc7514e1593926f7e017
Smrinov <wmforum@bk.ru>
2022-10-07 02:00:40 +0300

    Update lesson 265121

--- lesson/lesson_265121.py
+++ lesson/lesson_265121.py
@@ -1,3 +1,4 @@
+"""
 ar = []
 while True:
     text = input()
@@ -6,3 +7,55 @@ while True:
     else:
         break
 print(*ar, sep='\n')
+
+ar = []
+while True:
+    text = input()
+    if text.upper() != 'КОНЕЦ':
+        ar.append(text)
+    else:
+        break
+print(*ar, sep='\n')
+
+count = 0
+while True:
+    text = input()
+    if text in ['стоп', 'хватит', 'достаточно']:
+        break
+    count += 1
+print(count)
+
+ar = []
+while True:
+    n = int(input())
+    if n % 7:
+        break
+    ar.append(n)
+print(*ar, sep='\n')
+
+ar = []
+while True:
+    n = int(input())
+    if n < 0:
+        break
+    ar.append(n)
+print(sum(ar))
+
+count = 0
+while True:
+    n = int(input())
+    if n < 0 or n > 5:
+        break
+    if n == 5:
+        count += 1
+print(count)
+"""
+n = int(input())
+count = 0
+while n:
+    for i in (25, 10, 5, 1):
+        if n >= i:
+            count += 1
+            n -= i
+            break
+print(count)
