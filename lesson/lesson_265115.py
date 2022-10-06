0d71489f422e43cbca8f26afa926eb0e55f6adfb
KirillNN <wmforum@bk.ru>
2022-10-06 18:32:39 +0300

    Update lesson 265115 Add lesson 265110

--- lesson/lesson_265115.py
+++ lesson/lesson_265115.py
@@ -10,10 +10,31 @@ print(f'Футбольная команда {command} имеет длину {len
 city = [input() for x in range(3)]
 city.sort(key=len)
 print(city[0], city[2], sep='\n')
-"""
-x = [len(input()) for x in range(3)]
+
+x = [len(input()) for _ in range(3)]
 x.sort()
 res = 'NO'
 if x[0] - x[1] == x[1] - x[2]:
     res = 'YES'
 print(res)
+
+str = input()
+if 'синий' in str:
+    print('YES')
+else:
+    print('NO')
+
+str = input()
+if 'суббота' in str or 'воскресенье' in str:
+    print('YES')
+else:
+    print('NO')
+
+text = input()
+print("YES" if any(day in text for day in ["суббота", "воскресенье"]) else 'NO')
+"""
+text = input()
+if '.' in text and '@' in text:
+    print('YES')
+else:
+    print('NO')
