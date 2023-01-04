54330acd1f4d68789395c1266890adf5b889a5cf
Smrinov <wmforum@bk.ru>
2023-01-05 01:28:48 +0300

    Adds

--- lesson/lesson_284101.py
+++ lesson/lesson_284101.py
@@ -5,3 +5,47 @@
 #         print(value)
 
 # step 8
+# word = input()
+# for i in word[::-1]:
+#     print(i)
+
+# step 9
+# a, b, c = input(), input(), input()
+# print(b[0] + a[0] + c[0])
+
+# step 10
+# number = int(input())
+# summ = 0
+# while number > 0:
+#     summ += number % 10
+#     number //= 10
+# print(summ)
+
+# step 11
+# print('Цифра') if any([x.isdigit() for x in input()]) else print('Цифр нет')
+
+# step 12
+# word = input()
+# print(f'Символ + встречается {word.count("+")} раз')
+# print(f'Символ * встречается {word.count("*")} раз')
+
+# step 13
+# word = input()
+# count = 0
+# for i, j in zip(word, word[1:]):
+#     if i == j:
+#         count += 1
+# print(count)
+
+# step 14
+# sentence = input().lower()
+# vowel = ('а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е')
+# consonant = ('б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ')
+# count_vowel = sum(map(lambda x: x in vowel, sentence))
+# count_consonant = sum(map(lambda x: x in consonant, sentence))
+# print(f'Количество гласных букв равно {count_vowel}')
+# print(f'Количество согласных букв равно {count_consonant}')
+
+# step 15
+number = int(input())
+print(f'{number:b}')
---
+++ lesson/lesson_302627.py
@@ -0,0 +1,32 @@
+"""
+# step 7
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[:12])
+
+# step 8
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[-9:])
+
+# step 9
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[::7])
+
+# step 10
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[::-1])
+
+# step 11
+word = input()
+print('YES') if word == word[::-1] else print('NO')
+
+# step 12
+word = input()
+print(len(word))
+print(word * 3)
+print(word[0])
+print(word[:3])
+print(word[-3:])
+print(word[::-1])
+print(word[1:-1])
+"""
+# step 13
