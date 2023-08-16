0279c2d4436ec8d0bf90349eeb3af0e6112a819a
KirillNN <wmforum@bk.ru>
2023-08-17 01:04:28 +0300

    Update 334150

--- lesson/lesson_334150.py
+++ lesson/lesson_334150.py
@@ -6,11 +6,11 @@ def is_valid_triangle(a, b, c):
     return False
 
 
-# считываем данные
-a, b, c = int(input()), int(input()), int(input())
-
-# вызываем функцию
-print(is_valid_triangle(a, b, c))
+# # считываем данные
+# a, b, c = int(input()), int(input()), int(input())
+#
+# # вызываем функцию
+# print(is_valid_triangle(a, b, c))
 
 
 # step 3
@@ -24,11 +24,12 @@ def is_prime(num):
     return True
 
 
-# считываем данные
-n = int(input())
+# # считываем данные
+# n = int(input())
+#
+# # вызываем функцию
+# print(is_prime(n))
 
-# вызываем функцию
-print(is_prime(n))
 
 # step 4
 # Напишите функцию get_next_prime(num), которая принимает в качестве аргумента натуральное число num
@@ -36,14 +37,83 @@ print(is_prime(n))
 # объявление функции
 
 def get_next_prime(num):
-    while True: # TODO: доделать
-        for i in range(num, int(num ** 0.5) + 1):
+    while True:
+        num += 1
+        for i in range(2, int(num ** 0.5) + 1):
             if not num % i:
-                return False
-        return True
+                break
+        else:
+            return num
+
 
 # считываем данные
-n = int(input())
+# n = int(input())
+#
+# # вызываем функцию
+# print(get_next_prime(n))
+
+
+# step 5
+# Напишите функцию is_password_good(password), которая принимает
+# в качестве аргумента строковое значение пароля password
+# и возвращает значение True, если пароль является надежным
+# и False - в противном случае.
+# объявление функции
+def is_password_good(password):
+    flag = True
+    if len(password) < 8:
+        flag = False
+    elif not any([x.isupper() for x in password]):
+        flag = False
+    elif not any([x.islower() for x in password]):
+        flag = False
+    elif not any([x.isdigit() for x in password]):
+        flag = False
+    return flag
+
+
+# считываем данные
+# txt = input()
 
 # вызываем функцию
-print(get_next_prime(n))
+# print(is_password_good(txt))
+
+
+# step 6
+# Напишите функцию is_one_away(word1, word2), которая принимает
+# в качестве аргументов два слова word1 и word2 и возвращает
+# значение True, если слова имеют одинаковую длину и
+# отличаются ровно в 1 символе и False - в противном случае.
+# объявление функции
+def is_one_away(word1, word2):
+    if len(word1) != len(word2):
+        return False
+    elif [x == y for x, y in zip(word1, word2)].count(False) != 1:
+        return False
+    return True
+
+
+# считываем данные
+# txt1 = 'bike'
+# txt2 = 'hike'
+#
+# # вызываем функцию
+# print(is_one_away(txt1, txt2))
+
+
+# step 7
+# Напишите функцию is_palindrome(text), которая принимает в качестве
+# аргумента строку text и возвращает значение True если
+# указанный текст является палиндромом и False в противном случае.
+
+# объявление функции
+def is_palindrome(text):
+    text = [x.lower() for x in text if x.isalpha()]
+    return text == text[::-1]
+
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(is_palindrome(txt))
