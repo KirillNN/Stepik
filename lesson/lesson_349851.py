df94ea87eb4d88bd3ad6498fab80a60589677c8d
KirillNN <wmforum@bk.ru>
2023-08-20 13:45:17 +0300

    Update

---
+++ lesson/lesson_349851.py
@@ -0,0 +1,14 @@
+# step 3
+
+# number = '111111'
+# result = 0
+# for i, v in enumerate(number):
+#     if v == '1':
+#         result += 2**i
+# print(result)
+
+# step 10
+number = int(input())
+print(bin(number)[2:])
+print(oct(number)[2:])
+print(hex(number)[2:].upper())
--- lesson/lesson_352860.py
+++ lesson/lesson_352860.py
@@ -2,19 +2,27 @@
 # alphabet = ["а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о",
 #             "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
 
-alphabet = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
-# text = "Блажен, кто верует, тепло ему на свете!"
-text = "To be, or not to be, that is the question!"
-result = ""
-for i in text:
-    if i.lower() in alphabet:
-        index = alphabet.index(i.lower())
-        # print(index)
-        new_index = (index + 17) % len(alphabet)
-        if i.isupper():
-            result += alphabet[new_index].upper()
+# alphabet = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
+from string import ascii_lowercase as alphabet
+
+
+def decode(text, shift):
+    result = ""
+    for i in text:
+        if i.lower() in alphabet:
+            index = alphabet.index(i.lower())
+            new_index = (index + shift) % len(alphabet)
+            if i.isupper():
+                result += alphabet[new_index].upper()
+            else:
+                result += alphabet[new_index]
         else:
-            result += alphabet[new_index]
-    else:
-        result += i
-print(result)
+            result += i
+    return result
+
+
+user_input = input().split()
+output_list = []
+for word in user_input:
+    output_list.append(decode(word, len([x for x in word if x.isalpha()])))
+print(' '.join(output_list))
