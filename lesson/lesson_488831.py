e705ec8efd4224548cc9ddb36b5c0e86148e92ec
Kirill <wmforum@bk.ru>
2024-05-06 17:10:57 +0300

    update lesson_488831

--- lesson/lesson_488831.py
+++ lesson/lesson_488831.py
@@ -15,10 +15,37 @@ a = input()
 b = input()
 
 print(['NO', 'YES'][sorted(a) == sorted(b)])
-'''
+
 # step 3
 a = [x for x in input().lower() if x.isalpha()]
 b = [x for x in input().lower() if x.isalpha()]
 
-
 print(['NO', 'YES'][sorted(a) == sorted(b)])
+
+# step 4
+data = {}
+for _ in range(int(input())):
+    k, v = input().split()
+    data[k] = v
+
+word = input()
+for k, v in data.items():
+    if word == k:
+        print(v)
+        break
+    elif word == v:
+        print(k)
+        break
+'''
+# step 5
+data = {}
+for _ in range(int(input())):
+    k, *v = input().split()
+    data[k] = v
+
+for _ in range(int(input())):
+    city = input()
+    for k, v in data.items():
+        if city in v:
+            print(k)
+            break
