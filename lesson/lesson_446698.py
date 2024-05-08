d7229f59343a6e6a5ed1a58bdfade9a3e87f77b2
Kirill <wmforum@bk.ru>
2024-05-08 22:41:08 +0300

    add lesson_446698

---
+++ lesson/lesson_446698.py
@@ -0,0 +1,66 @@
+'''
+# step 6
+numbers = [34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95, 1, 36, -38, -19, 1, 6, 87]
+
+result = {i: v ** 2 for i, v in enumerate(numbers)}
+
+# step 7
+colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange',
+          'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber',
+          'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None,
+          'c22': 'Sand', 'c23': None}
+
+result = {i: v for i, v in colors.items() if v}
+
+# step 8
+favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62,
+                    'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654,
+                    'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271,
+                    'anna': 55, 'madlen': 876}
+
+result = {i: v for i, v in favorite_numbers.items() if 9 < v < 100}
+
+# step 9
+months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August',
+          9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+result = {v: i for i, v in months.items()}
+
+# step 10
+s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
+result = {}
+for i in s.split():
+    x, y = i.split(":")
+    result[int(x)] = y
+
+result = {int(x): y for x, y in (i.split(':') for i in s.split())}
+print(result)
+# step 11
+numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
+
+result = {x: [i for i in range(1, x + 1) if not x % i] for x in numbers}
+print(result)
+
+# step 12
+words = ['hello', 'bye', 'yes', 'no', 'python', 'apple', 'maybe', 'stepik', 'beegeek']
+
+result = {x: [ord(i) for i in x] for x in words}
+print(result)
+# step 13
+letters = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M',
+           13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y',
+           26: 'Z'}
+
+remove_keys = [1, 5, 7, 12, 17, 19, 21, 24]
+
+result = {i: v for i, v in letters.items() if i not in remove_keys}
+print(result)
+'''
+# step 14
+students = {'Timur': (170, 75), 'Ruslan': (180, 105), 'Soltan': (192, 68), 'Roman': (175, 70), 'Madlen': (160, 50),
+            'Stefani': (165, 70), 'Tom': (190, 90), 'Jerry': (180, 87), 'Anna': (172, 67), 'Scott': (168, 78),
+            'John': (186, 79), 'Alex': (195, 120), 'Max': (200, 110), 'Barak': (180, 89), 'Donald': (170, 80),
+            'Rustam': (186, 100), 'Alice': (159, 59), 'Rita': (170, 80), 'Mary': (175, 69), 'Jane': (190, 80)}
+
+result = {i: (v, z) for i, (v, z) in students.items() if v > 167 and z < 75}
+print(result)
